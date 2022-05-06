918b0557bed0e60e94f9c0e09deeb4251199bae7
dmsophia <sophia.dias@sou.inteli.edu.br>
2022-05-06 11:26:29 -0300

    exercício obrigatório semana03

---
+++ SEMANA_03/04_AUT_EST_EX_OBRIGATORIOS/ex.js
@@ -0,0 +1,9 @@
+function getFuncao(){
+    velocidade = documento.getElementByld("velocidade").value;
+    alert(velocidade)
+    console.log(velocidade)
+    var tempo = velocidade/10
+    var altura = (velocidade * velocidade)/20
+    alert(tempo)
+    alert(altura)
+}
